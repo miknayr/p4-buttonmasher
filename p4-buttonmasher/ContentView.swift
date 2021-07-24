90283c2b3e9aadacc6c2929ecef9a18df06c8d24
miknayr <teamry4n@gmail.com>
2021-07-24 13:57:44 -0700

    Initial Commit

---
+++ p4-buttonmasher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  p4-buttonmasher
+//
+//  Created by Ryan Kim on 7/24/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ p4-buttonmasher/p4_buttonmasherApp.swift
@@ -0,0 +1,17 @@
+//
+//  p4_buttonmasherApp.swift
+//  p4-buttonmasher
+//
+//  Created by Ryan Kim on 7/24/21.
+//
+
+import SwiftUI
+
+@main
+struct p4_buttonmasherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
