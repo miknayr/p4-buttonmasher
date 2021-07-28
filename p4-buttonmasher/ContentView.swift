6b3f493cac0a20cf7d3c4be4cef005b8324eebb0
miknayr <teamry4n@gmail.com>
2021-07-28 11:24:26 -0700

    finished? not styled very well

--- p4-buttonmasher/ContentView.swift
+++ p4-buttonmasher/ContentView.swift
@@ -13,14 +13,19 @@ struct ContentView: View {
     @State var sliderValue = 5.0
     @State var target = Int.random(in: 1...10)
     @State var score = 0
-    @State var round = 1
+    @State var round = 2
     @State var invis = true
     @State var moves : [String] = Array(repeating: "", count: 9)
     // to identify the current player..
     @State var isPlaying = true
     @State var gameOver = false
     @State var msg = ""
-   
+    @State var timeRemaining = 10
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+
+ 
+    
     
     struct LableStyle: ViewModifier {
         func body(content: Content) -> some View {
@@ -29,38 +34,53 @@ struct ContentView: View {
                 .font(Font.custom("HelveticaNeue-Medium", size: 18))
         }
     }
+    struct CountdownStyle: ViewModifier {
+        func body(content: Content) -> some View {
+            return content
+                .foregroundColor(Color.black)
+                .font(Font.custom("HelveticaNeue-Medium", size: 40))
+        }
+    }
     
     var body: some View {
-    
+        
         VStack {
+            
+        
                 // ~~~~~~~~~~~~~~~~ inside for game two
                 if round == 1 {
                     
                     VStack {
                         HStack{
                             VStack {
-                                
-                                Text("The countdown:")
-                                
-                            }
-                            
-                        }
-                       
-                        
-                       
-                        // gridview for playing
-                        LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 15), count: 3), spacing: 15){
-                            ForEach(0..<9, id: \.self){index in
-                                ZStack {
-                                    //Flip animation..
-                                    Color.blue
-                                    Color.gray
-                                        .opacity(moves[index] == "" ? 1: 0)
-                                    Text(moves[index])
-                                        .font(.system(size:55))
-                                        .fontWeight(.heavy)
-                                        .foregroundColor(.black)
+
+                                Text("The countdown:").modifier(CountdownStyle())
+                                Text("\(timeRemaining)")
+                                   .onReceive(timer) { _ in
+                                       if timeRemaining > 0 {
+                                            timeRemaining -= 1
+                                    }
+                                       else if timeRemaining == 0 && round == 1 {
+                                            round -= 1
+                                       }
+
                                 }
+                            }
+                        }
+                                              
+                        // gridview for playing
+            LazyVGrid(columns: Array(repeating: GridItem(.flexible(), spacing: 15), count: 3), spacing: 15){
+                    ForEach(0..<9, id: \.self){index in
+                    ZStack {
+                                //Flip animation..
+                                Color.blue
+                                Color.gray
+                                    .opacity(moves[index] == "" ? 1: 0)
+                                Text(moves[index])
+                                    .font(.system(size:55))
+                                    .fontWeight(.heavy)
+                                    .foregroundColor(.black)
+                    }
                                 .frame(width: getWidth(), height: getWidth())
                                 .cornerRadius(15)
                                 // whenever tapped adding move
@@ -82,15 +102,31 @@ struct ContentView: View {
                         checkWinner()
                     })
                     .alert(isPresented: $gameOver, content: {
-                        Alert(title: Text("Winner"), message: Text(msg), dismissButton:
+                        Alert(title: Text("Winner"), message: Text("You scored \(pointsForCurrentRoundTTT()) points this round."), dismissButton:
                                 .destructive(Text("Play Again"), action:{
                                     // resetting all data
                                 
                                     moves.removeAll()
                                     moves = Array(repeating:"", count: 9)
+                                    
+                                    self.timeRemaining = 10
+                                    self.score = self.score + self.pointsForCurrentRoundTTT()
                                     self.round -= 1
+                                    
                                 }))
                     })
+//                    .alert(isPresented: $alertIsVisible) { () -> Alert in
+//
+//                        return Alert (title: Text(alertTitle()), message: Text(
+//                            msg + "You scored \(pointsForCurrentRoundTTT()) points this round."
+//                        ), dismissButton: .default(Text("Awesome!")) {
+//                            self.score = self.score + self.pointsForCurrentRoundTTT()
+//
+//
+//
+//
+//                        })
+//                    }
                     
                 }
                 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -99,6 +135,23 @@ struct ContentView: View {
                     Spacer()
 
                     // Target row
+                    VStack{
+                        Text("The countdown:").modifier(CountdownStyle())
+                        Text("\(timeRemaining)")
+                           .onReceive(timer) { _ in
+                               if timeRemaining > 0 {
+                                    timeRemaining -= 1
+                            }
+                               else if timeRemaining == 0 {
+                                    round -= 1
+                                    timeRemaining = 10
+                                    
+                               }
+                          
+                        }
+                    }
+                    Spacer()
+                    
                     HStack {
                         Text("Put the bullseye as close as you can to:").modifier(LableStyle())
                         Text("\(target)")
@@ -120,41 +173,76 @@ struct ContentView: View {
                     Button(action: {
                         print("buton pressed!, \(score)" )
                         self.alertIsVisible = true
-
+                        self.score = self.score + self.pointsForCurrentRound()
+                        self.target = Int.random(in: 1...10)
+                        self.round -= 1
+                        self.sliderValue = 5.00
+                       
+                        
                     }) {
-                        Text("Hit Me!").modifier(LableStyle())
-
-                    }
-                    .alert(isPresented: $alertIsVisible) { () -> Alert in
-
-                        return Alert (title: Text(alertTitle()), message: Text(
-                            "The slider's value is \(sliderValueRounded()).\n" +
-                            "You scored \(pointsForCurrentRound()) points this round."
-                        ), dismissButton: .default(Text("Awesome!")) {
-                            self.score = self.score + self.pointsForCurrentRound()
-                            self.target = Int.random(in: 1...10)
-                            self.round -= 1
-                            self.sliderValue = 5.00
+                        Text("       ").modifier(LableStyle())
+                            .background(Image("button").scaleEffect(0.3))
                            
-
-                        })
                     }
+                    
+                    
+                   
+//                    .alert(isPresented: $alertIsVisible) { () -> Alert in
+//
+//                        return Alert (title: Text(alertTitle()), message: Text(
+//                            "The slider's value is \(sliderValueRounded()).\n" +
+//                            "You scored \(pointsForCurrentRound()) points this round."
+//                        ), dismissButton: .default(Text("Awesome!")) {
+//                            self.score = self.score + self.pointsForCurrentRound()
+//                            self.target = Int.random(in: 1...10)
+//                            self.round -= 1
+//                            self.sliderValue = 5.00
+//                            timeRemaining = 10
+//
+//
+//                        })
+//                    }
                     Spacer()
 
 
                 }
    
                 if round == 0 {
-                    Text("Finished!").font(Font.custom("HelveticaNeue-Medium", size: 58))
+                    
+                    Spacer()
+                    VStack{
+                        Text("Finished!").font(Font.custom("HelveticaNeue-Medium", size: 58))
+                        Text("Start a new game!").font(Font.custom("HelveticaNeue-Medium", size: 15))
+                        Text("")
+                        Text("")
+                    }
+                   
+                    VStack{
+                        Text("Your Score is \(score)").font(Font.custom("HelveticaNeue", size: 20))
+                    }
+                    Spacer()
                 }
+            
+                if round > 4 {
+                    Spacer()
+                    Text("Button Masher").font(Font.custom("HelveticaNeue-Medium", size: 40))
+                    Text("Made by Ryan K!").font(Font.custom("HelveticaNeue-Medium", size: 15))
+                    Spacer()
+                    
+                }
+           
 
             // Score row work above here ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
             HStack {
 
-
+        
                 Button(action: {
 
                     startNewGame()
+                    timeRemaining = 10
+                    moves.removeAll()
+                    moves = Array(repeating:"", count: 9)
                 }) {
                     Text("Start Over")
     
@@ -166,14 +254,17 @@ struct ContentView: View {
                 Text("Round:")
                 Text("\(round)")
                 Spacer()
-                Button(action:{ }) { // create the view, push the view controller somehow.
-                    Text("Info")
+                Button(action:{
+                    round = 10
+                }) { // create the view, push the view controller somehow.
+                    Text(" ")
+                        .background(Image("info").scaleEffect(0.4))
                 }
 
 
             }
             .padding(.bottom, 30)
-            .padding(.horizontal, 20)
+            .padding(.horizontal, 40)
            
         }
 //        .background(Image("grasstexture3"), alignment: .center)
@@ -184,8 +275,13 @@ struct ContentView: View {
         Int(sliderValue.rounded())
     }
 
+
     func amountOff() -> Int {
         abs(target - sliderValueRounded())
+    }
+    
+    func amountOffTTT() -> Int {
+        abs(10 - timeRemaining)
     }
     func pointsForCurrentRound() -> Int {
         let maximumScore = 10
@@ -200,6 +296,31 @@ struct ContentView: View {
         }
         return maximumScore - difference + bonus
     }
+    
+    func pointsForCurrentRoundTTT() -> Int {
+        let maximumScore = 10
+        let difference = amountOff()
+        let bonus: Int
+        if difference == 1 {
+            bonus = 30
+        } else if difference == 2 {
+            bonus = 20
+        } else if difference == 3 {
+            bonus = 10
+        } else if difference == 4 {
+            bonus = 7
+        } else if difference == 5 {
+            bonus = 5
+        } else if difference == 6 {
+            bonus = 3
+        } else if difference == 7 {
+            bonus = 2
+        }  else {
+            bonus = 0
+        }
+        return maximumScore - difference + bonus
+    }
+    
     func alertTitle() -> String {
         let difference = amountOff()
         let title: String
@@ -280,6 +401,7 @@ struct ContentView: View {
         return false
     }
  
+
 }
 struct ContentView_Previews:
     PreviewProvider {
